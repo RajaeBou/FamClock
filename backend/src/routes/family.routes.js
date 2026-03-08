375ea81c24a86f5ad153faade3209c9df75bd562
RajaeBou <r.bouziani@students.ephec.be>
2026-03-08 17:19:50 +0100

    Revert "Merge pull request #39 from RajaeBou/feature/us-1.4-delete-member"
    This reverts commit c0f7bd9496d3275e5b89189f2c0547955a10bd0e, reversing
    changes made to cdee619c307176a4b74cdf6a34eb49b8e13fc636.

--- backend/src/routes/family.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createFamily, loginFamily } = require("../controllers/family.controller");
-
-router.post("/", createFamily);
-router.post("/login", loginFamily);
-
-module.exports = router;
