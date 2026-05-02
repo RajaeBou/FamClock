88b3b0987a9b971327882d3118cdae6c5b1aa815
RajaeBou <r.bouziani@students.ephec.be>
2026-05-02 19:03:49 +0200

    Appeler initDb au démarrage du serveur

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
-// Création automatique des tables SQLite au démarrage
-require("./database/initDb");
-
+const initDb = require("./database/initDb");
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
+
+initDb();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
